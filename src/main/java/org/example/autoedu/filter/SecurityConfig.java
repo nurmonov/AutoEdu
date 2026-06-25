c687eda151abc433ddc11cc65ae62e76133f4406
nurmonov <nurmonoff@gmail.com>
2026-06-25 16:42:37 +0500

    commit

--- src/main/java/org/example/autoedu/filter/SecurityConfig.java
+++ src/main/java/org/example/autoedu/filter/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 🔥 MUHIM
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
